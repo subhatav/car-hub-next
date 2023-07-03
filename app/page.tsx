1c3577fd13a3aced4e10127b8a09cc5dea9125b4
Subhatav Dhali <subhatav@gmail.com>
2023-07-03 21:45:58 +0530

    Adapt the `page` TSX file for client-side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+
 import { fuels, yearsOfProduction } from "@/constants";
 
-import { HomeProps } from "@/types";
 import { fetchCars } from "@/utils";
 
 import Hero from "@/components/Hero";
@@ -9,16 +14,44 @@ import CustomFilter from "@/components/CustomFilter";
 import CarCard from "@/components/CarCard";
 import ShowMore from "@/components/ShowMore";
 
-export default async function Home({ searchParams }: HomeProps) {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || "",
-    limit: searchParams.limit || 10,
-    model: searchParams.model || "",
-  });
+export default function Home() {
+  const [allCars, setAllCars] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+  // Search States
+  const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
+
+  // Filter States
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
+
+  // Pagination State
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async () => {
+    setLoading(true);
+
+    try {
+      const result = await fetchCars({
+        manufacturer: manufacturer || "",
+        model: model || "",
+        fuel: fuel || "",
+        year: year || 2022,
+        limit: limit || 10,
+      });
+
+      setAllCars(result);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCars();
+  }, [fuel, year, limit, manufacturer, model]);
 
   return (
     <main className="overflow-hidden">
@@ -31,14 +64,18 @@ export default async function Home({ searchParams }: HomeProps) {
         </div>
 
         <div className="home__filters">
-          <SearchBar />
+          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} />
-            <CustomFilter title="year" options={yearsOfProduction} />
+            <CustomFilter title="fuel" options={fuels} setFilter={setFuel} />
+            <CustomFilter
+              title="year"
+              options={yearsOfProduction}
+              setFilter={setYear}
+            />
           </div>
         </div>
 
-        {!isDataEmpty ? (
+        {allCars.length > 0 ? (
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (
@@ -46,9 +83,22 @@ export default async function Home({ searchParams }: HomeProps) {
               ))}
             </div>
 
+            {loading && (
+              <div className="mt-16 w-full flex-center">
+                <Image
+                  src="/loader.svg"
+                  alt="Loader Icon"
+                  width={50}
+                  height={50}
+                  className="object-contain"
+                />
+              </div>
+            )}
+
             <ShowMore
-              pageNumber={(searchParams.limit ?? 10) / 10}
-              isNext={(searchParams.limit ?? 10) > allCars.length}
+              pageNumber={limit / 10}
+              isNext={limit > allCars.length}
+              setLimit={setLimit}
             />
           </section>
         ) : (
