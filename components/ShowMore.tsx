7a6438c31f13822f8c3ffba7e31d79b8af00c82b
Subhatav Dhali <subhatav@gmail.com>
2023-07-03 21:51:36 +0530

    Adapt the `ShowMore` TSX file according to the transformation

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
-import { updateSearchParams } from "@/utils";
 import { ShowMoreProps } from "@/types";
 
 import CustomButton from "./CustomButton";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-  const router = useRouter();
-
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
   const handleNavigation = () => {
     // Calculate the new limit based on the Page Number
     const freshLimit = (pageNumber + 1) * 10;
 
-    // Update the "limit" Search Parameter in the URL with the new value
-    const freshPathname = updateSearchParams("limit", `${freshLimit}`);
-
-    router.push(freshPathname);
+    setLimit(freshLimit);
   };
 
   return (
